be0aa3844336958661e80e8b1610d12dceddb958
Daniel De Vera <daniel@signadot.com>
2025-07-08 15:06:41 -0300

    Add traffic manager support in go-routesapi

--- go-routesapi/workload/router.go
+++ go-routesapi/workload/router.go
@@ -8,16 +8,20 @@ import (
 	"github.com/signadot/routesapi/go-routesapi/watched"
 )
 
-type Router interface {
-	GetTarget(containerPort int32, rks ...string) string
-	GetTargetWithContext(ctx context.Context, containerPort int32, rks ...string) (string, error)
-}
-
 type router struct {
 	watched watched.BaselineWatched
 }
 
 func NewRouter(cfg *Config) (Router, error) {
+	var cliInfo *routesapi.ClientInfo
+	if cfg.ClientInfo != nil {
+		cliInfo = cfg.ClientInfo
+	} else {
+		// by default support virtual workloads
+		cliInfo = &routesapi.ClientInfo{
+			EnableVirtualWorkloads: true,
+		}
+	}
 	w, err := watched.NewBaselineWatched(context.Background(),
 		&watched.Config{
 			Addr: cfg.RouteServerAddr,
@@ -27,23 +31,24 @@ func NewRouter(cfg *Config) (Router, error) {
 			Kind:      cfg.Baseline.Kind,
 			Namespace: cfg.Baseline.Namespace,
 			Name:      cfg.Baseline.Name,
-		})
+		},
+		cliInfo)
 	if err != nil {
 		return nil, err
 	}
 	return &router{w}, nil
 }
 
-func (r *router) GetTarget(containerPort int32, rks ...string) string {
+func (r *router) GetTarget(containerPort int32, rks ...string) *Target {
 	res, _ := r.GetTargetWithContext(context.Background(), containerPort, rks...)
 	return res
 }
 
-func (r *router) GetTargetWithContext(ctx context.Context, containerPort int32, rks ...string) (string, error) {
+func (r *router) GetTargetWithContext(ctx context.Context, containerPort int32, rks ...string) (*Target, error) {
 	for _, rk := range rks {
 		rr, err := r.watched.GetWithContext(ctx, rk)
 		if err != nil {
-			return "", err
+			return nil, err
 		}
 		if rr == nil {
 			continue
@@ -53,12 +58,25 @@ func (r *router) GetTargetWithContext(ctx context.Context, containerPort int32, 
 			if int32(pr.WorkloadPort) != containerPort {
 				continue
 			}
-			if len(pr.Destinations) == 0 {
+			if len(pr.Destinations) == 0 && (pr.TrafficManager == nil ||
+				len(pr.TrafficManager.NextDestinations) == 0) {
 				continue
 			}
-			dest := pr.Destinations[0]
-			return fmt.Sprintf("%s:%d", dest.Host, dest.Port), nil
+
+			var target Target
+			if len(pr.Destinations) > 0 {
+				dest := pr.Destinations[0]
+				target.Destination = fmt.Sprintf("%s:%d", dest.Host, dest.Port)
+			}
+			if pr.TrafficManager != nil && len(pr.TrafficManager.NextDestinations) > 0 {
+				dest := pr.TrafficManager.NextDestinations[0]
+				target.TrafficManager = &TrafficManager{
+					NextDestination: fmt.Sprintf("%s:%d", dest.Host, dest.Port),
+					AllTraffic:      pr.TrafficManager.AllTraffic,
+				}
+			}
+			return &target, nil
 		}
 	}
-	return "", nil
+	return nil, nil
 }
